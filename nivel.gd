9eb3cda0aaaa486e0ecfb7d01b9440189c7bd9ba
pradoh97 <pradoh97@gmail.com>
2025-10-22 10:08:17 -0300

    sumar puntaje y agregar gema

--- nivel.gd
+++ nivel.gd
@@ -5,3 +5,8 @@ var puntaje = 0
 func _on_moneda_body_entered(body: Node2D) -> void:
 	puntaje = puntaje + 1
 	print(puntaje)
+
+
+func _on_gema_body_entered(body: Node2D) -> void:
+	puntaje = puntaje + 10
+	print(puntaje)
